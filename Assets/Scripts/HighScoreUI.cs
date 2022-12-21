482a836f1eb0e05310024a254f82efdc656db14d
rokic <rokic.github@gmail.com>
2022-12-22 06:27:57 +0800

    feat: new button to clear high scores

--- Assets/Scripts/HighScoreUI.cs
+++ Assets/Scripts/HighScoreUI.cs
@@ -11,22 +11,35 @@ public class HighScoreUI : MonoBehaviour
 
     void Start()
     {
+        UpdateScoreList();
+    }
+
+    void UpdateScoreList() {
         List<GameManager.ScoreRecord> scores = GameManager.Instance.scores;
         int disableLines = 0;
 
         for (int i = 0; i < scoreTexts.Length; i++)
         {
-            if (i >= scores.Count) {
+            if (i >= scores.Count)
+            {
                 scoreTexts[i].gameObject.SetActive(false);
                 disableLines++;
                 continue;
-            } 
+            }
 
             GameManager.ScoreRecord record = scores[i];
+
+            scoreTexts[i].gameObject.SetActive(true);
             scoreTexts[i].text = $"{record.name}: {record.score}";
         }
 
         scorePanel.sizeDelta = new Vector2(400, 450 - disableLines * 30);
+    }
+
+    public void ClearAll()
+    {
+        GameManager.Instance.ClearScores();
+        UpdateScoreList();
     }
 
     public void BackToMenu()
