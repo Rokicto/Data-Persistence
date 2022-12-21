ff5fcbd8cad8f33b219e6b9168ea6dca4b2cb525
rokic <rokic.github@gmail.com>
2022-12-22 04:39:13 +0800

    feat: high scores scene

---
+++ Assets/Scripts/HighScoreUI.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HighScoreUI : MonoBehaviour
+{
+    public RectTransform scorePanel;
+    public Text[] scoreTexts;
+
+    void Start()
+    {
+        List<GameManager.ScoreRecord> scores = GameManager.Instance.scores;
+        int disableLines = 0;
+
+        for (int i = 0; i < scoreTexts.Length; i++)
+        {
+            if (i >= scores.Count) {
+                scoreTexts[i].gameObject.SetActive(false);
+                disableLines++;
+                continue;
+            } 
+
+            GameManager.ScoreRecord record = scores[i];
+            scoreTexts[i].text = $"{record.name}: {record.score}";
+        }
+
+        scorePanel.sizeDelta = new Vector2(400, 450 - disableLines * 30);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("startmenu");
+    }
+}
